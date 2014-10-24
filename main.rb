34b506f84cdb3c4c2b1f801b26897971cfe0a96e
Vince Francesi <vincefrancesi@gmail.com>
2014-10-24 00:22:14 -0700

    Cleanup some testing code

--- main.rb
+++ main.rb
@@ -6,10 +6,11 @@ TEST        = "ABCD"
 
 fittest = []
 
+puts "Generation, Fittest 0, Fittest 1"
 GENERATIONS.times do |i|
   population = []
 
-  (POPULATION - population.size).times do
+  POPULATION.times do
     if fittest.size > 0
       population << fittest[0].reproduce(fittest[1])
     else
@@ -19,10 +20,14 @@ GENERATIONS.times do |i|
 
   population = population.sort_by { |o| o.test(TEST) }
 
-  fittest = [population[0].dup, population[1].dup]
+  fittest = [population[0], population[1]]
+
+  # CSV output for graphing
   puts "#{i},#{fittest[0].test(TEST)},#{fittest[1].test(TEST)}"
+
   break if fittest[0].test(TEST) == 0
 end
 
+puts "Resulting genomes:"
 puts fittest[0].genome
 puts fittest[1].genome
